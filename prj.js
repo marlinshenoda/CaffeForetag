39e5d87190e2f40b1dfed1c09818909aeae56e5a
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2022-06-17 20:52:29 +0200

    first commit

---
+++ prj.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function(){
+
+  $("#Company").click(function(){
+   
+    $("#con1").css("display","block");
+ $("#con2").css("display","none");
+});
+
+});
+
+  $(document).ready(function(){
+    $("#PrivatePerson").click(function(){
+    
+  
+   $("#con2").css("display","block");
+   $("#con1").css("display","none");
+  });
+   
+});
+
+$(document).ready(function(){
+  $("#details").click(function(){
+  
+
+ alert("Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, tempora! ")
+});
+ 
+});
+
+  
+ 
+
+
